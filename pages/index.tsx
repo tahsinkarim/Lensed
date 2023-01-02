0e3da0e129685d017f0f5999b135eb51712fd25d
Tahsin Karim <tahsinkarim.dev@gmail.com>
2023-01-02 13:14:43 +0600

    api test

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,7 +40,7 @@ const Home: NextPage<IProps> = ({ images }) => {
 export default Home;
 
 export const getServerSideProps = async () => {
-  const res = await axios.get(`https://lensed-psi.vercel.app/api/post`);
+  const res = await axios.get(`/api/post`);
   const data = await res.data;
 
   return {
